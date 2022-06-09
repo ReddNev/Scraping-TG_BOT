a4589dd36a1c3aaec598ee153053a3c9aa9df87b
ReddNev <creedlll@mail.ru>
2022-06-09 19:36:10 +0300

    Show the fresh news

--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,7 @@ import json
 import telebot
 
 from config import TOKEN
+from main import check_news
 
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
@@ -24,6 +25,21 @@ def get_five_news(message: telebot.types.Message):
                f"{v['article_url']}"
 
         bot.reply_to(message, news)
+
+
+@bot.message_handler(commands=['fresh_news'])
+def get_five_news(message: telebot.types.Message):
+    fresh_news = check_news()
+
+    if len(fresh_news) >= 1:
+        for k, v in sorted(fresh_news.items()):
+            news = f"{v['article_data_time']}" \
+                   f"{v['article_title']} " \
+                   f"{v['article_url']}"
+
+            bot.reply_to(message, news)
+    else:
+        bot.reply_to(message, text='no news')
 
 
 if __name__ == '__main__':
