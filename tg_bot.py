282ef7a5c96089103dd826c541060b56cf584383
ReddNev <creedlll@mail.ru>
2022-06-09 19:32:41 +0300

    Show the first 5 new

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,8 @@
+import json
 import telebot
 
 from config import TOKEN
+
 
 bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 
@@ -9,6 +11,19 @@ bot = telebot.TeleBot(TOKEN, parse_mode='HTML')
 def start(message: telebot.types.Message):
 
     bot.reply_to(message, text='News feed')
+
+
+@bot.message_handler(commands=['last_five'])
+def get_five_news(message: telebot.types.Message):
+    with open("news_dict.json", encoding='utf-8') as file:
+        news_dict = json.load(file)
+
+    for k, v in sorted(news_dict.items())[-5:]:
+        news = f"<b>{v['article_data_time']}</b>\n" \
+               f"<u>{v['article_title']}</u>\n" \
+               f"{v['article_url']}"
+
+        bot.reply_to(message, news)
 
 
 if __name__ == '__main__':
